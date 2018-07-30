26b1d40fbf517f394d4627b86064c80d6b54d979
John Kirkland <13470641+ghjpk@users.noreply.github.com>
2018-07-30 14:24:41 -0500

    Resolved Issues #1 and #2 from github

--- forms/selectplanform.py
+++ forms/selectplanform.py
@@ -160,120 +160,122 @@ class SelectPlanForm(QtWidgets.QDialog, Ui_SelectPlanDialog):
         Create a new service and import all of the PCO items into it
         """
         service_manager = Registry().get('service_manager')
-        service_manager.application.set_busy_cursor()
-        # get the plan ID for the current plan selection
-        plan_id = self.plan_selection_combo_box.itemData(self.plan_selection_combo_box.currentIndex())
-        # get the items array from Planning Center
-        planning_center_items_dict = self.planning_center_api.GetItemsDict(plan_id)
-
         old_service_items = []
         if update:
             old_service_items = service_manager.service_items.copy()
-            service_manager.service_items = []
+            service_manager.new_file()
         else:
-            service_manager.on_new_service_clicked()
-        
-        service_manager.main_window.display_progress_bar(len(planning_center_items_dict['data']))
-        # convert the planning center dict to Songs and Add them to the ServiceManager
-        pco_id_to_openlp_id = {}
-        for item in planning_center_items_dict['data']:
-            item_title = item['attributes']['title']
-            media_type = ''
-            openlp_id = -1
-            if item['attributes']['item_type'] == 'song':
-                arrangement_id = item['relationships']['arrangement']['data']['id']
-                song_id = item['relationships']['song']['data']['id']
-                if song_id not in pco_id_to_openlp_id:
-                    # get arrangement from "included" resources
-                    arrangement_data = {}
-                    song_data = {}
-                    for included_item in planning_center_items_dict['included']:
-                        if included_item['type'] == 'Song' and included_item['id'] == song_id:
-                            song_data = included_item
-                        elif included_item['type'] == 'Arrangement' and included_item['id'] == arrangement_id:
-                            arrangement_data = included_item
-                        # if we have both song and arrangement set, stop iterating
-                        if len(song_data) and len(arrangement_data):
-                            break
-                    author = song_data['attributes']['author']
-                    lyrics = arrangement_data['attributes']['lyrics']
-                    arrangement_updated_at = datetime.strptime(arrangement_data['attributes']['updated_at'], '%Y-%m-%dT%H:%M:%SZ')
-                    # start importing the song
-                    pco_import = PlanningCenterSongImport()
-                    theme_name = self.song_theme_selection_combo_box.currentText()
-                    openlp_id = pco_import.add_song(item_title,author,lyrics,theme_name,arrangement_updated_at)
-                    pco_id_to_openlp_id[song_id] = openlp_id
-
-                openlp_id = pco_id_to_openlp_id[song_id]
-                media_type = 'songs'
-            else:
-                """ 
-                PCO has deprecated their custom slide interface and they are 
-                planning to eliminate it in mid-2019, so this interface will not 
-                support their custom slides except to create a custom slide 
-                placeholder with just the title
-                """
-                theme_name = self.slide_theme_selection_combo_box.currentText()
-                openlp_id = PlanningCenterCustomImport(item_title,theme_name)
-                media_type = 'custom'
-            
-            # get the current values of these values so I can set them back to this value
-            media_type_plugin = Registry().get(media_type)
-            # the variable suffix names below for "songs" is "song", so change media_type to song
-            media_type_suffix = media_type
-            if media_type == 'songs':
-                media_type_suffix = 'song'
-            previous_remote_triggered = media_type_plugin.remote_triggered
-            previous_remote_value = getattr(media_type_plugin, "remote_{0}".format(media_type_suffix))
-            # turn on remote song feature to add to service
-            media_type_plugin.remote_triggered = True
-            setattr(media_type_plugin,"remote_{0}".format(media_type_suffix), openlp_id)
-            media_type_plugin.add_to_service(remote=openlp_id)
-            # reset values to their previous values
-            media_type_plugin.remote_triggered = previous_remote_triggered
-            setattr(media_type_plugin,"remote_{0}".format(media_type_suffix), previous_remote_value)
-            service_manager.main_window.increment_progress_bar()
-                    
-        if update:
-            for old_service_item in old_service_items:
-                # see if this service_item contained within the current set of service items
+            blah=service_manager.on_new_service_clicked()
+            print(blah)
+        # we only continue here if the service_manager is now empty
+        if len(service_manager.service_items) == 0:
+            service_manager.application.set_busy_cursor()
+            # get the plan ID for the current plan selection
+            plan_id = self.plan_selection_combo_box.itemData(self.plan_selection_combo_box.currentIndex())
+            # get the items array from Planning Center
+            planning_center_items_dict = self.planning_center_api.GetItemsDict(plan_id)
+            service_manager.main_window.display_progress_bar(len(planning_center_items_dict['data']))
+            # convert the planning center dict to Songs and Add them to the ServiceManager
+            pco_id_to_openlp_id = {}
+            for item in planning_center_items_dict['data']:
+                item_title = item['attributes']['title']
+                media_type = ''
+                openlp_id = -1
+                if item['attributes']['item_type'] == 'song':
+                    arrangement_id = item['relationships']['arrangement']['data']['id']
+                    song_id = item['relationships']['song']['data']['id']
+                    if song_id not in pco_id_to_openlp_id:
+                        # get arrangement from "included" resources
+                        arrangement_data = {}
+                        song_data = {}
+                        for included_item in planning_center_items_dict['included']:
+                            if included_item['type'] == 'Song' and included_item['id'] == song_id:
+                                song_data = included_item
+                            elif included_item['type'] == 'Arrangement' and included_item['id'] == arrangement_id:
+                                arrangement_data = included_item
+                            # if we have both song and arrangement set, stop iterating
+                            if len(song_data) and len(arrangement_data):
+                                break
+                        author = song_data['attributes']['author']
+                        lyrics = arrangement_data['attributes']['lyrics']
+                        arrangement_updated_at = datetime.strptime(arrangement_data['attributes']['updated_at'], '%Y-%m-%dT%H:%M:%SZ')
+                        # start importing the song
+                        pco_import = PlanningCenterSongImport()
+                        theme_name = self.song_theme_selection_combo_box.currentText()
+                        openlp_id = pco_import.add_song(item_title,author,lyrics,theme_name,arrangement_updated_at)
+                        pco_id_to_openlp_id[song_id] = openlp_id
+    
+                    openlp_id = pco_id_to_openlp_id[song_id]
+                    media_type = 'songs'
+                else:
+                    """ 
+                    PCO has deprecated their custom slide interface and they are 
+                    planning to eliminate it in mid-2019, so this interface will not 
+                    support their custom slides except to create a custom slide 
+                    placeholder with just the title
+                    """
+                    theme_name = self.slide_theme_selection_combo_box.currentText()
+                    openlp_id = PlanningCenterCustomImport(item_title,theme_name)
+                    media_type = 'custom'
                 
-                # see if we have this same value in the new service
-                for service_index,service_item in enumerate(service_manager.service_items):
-                    # we can compare songs to songs and custom to custom but not between them
-                    if old_service_item['service_item'].name == 'songs' and service_item['service_item'].name == 'songs':
-                        if old_service_item['service_item'].audit == service_item['service_item'].audit:
-                            # get the timestamp from the xml of both the old and new and compare... 
-                            # modifiedDate="2018-06-30T18:44:35Z"
-                            old_match = re.search('modifiedDate="(.+?)Z*"', old_service_item['service_item'].xml_version)
-                            old_datetime = datetime.strptime(old_match.group(1), '%Y-%m-%dT%H:%M:%S')
-                            new_match = re.search('modifiedDate="(.+?)Z*"', service_item['service_item'].xml_version)
-                            new_datetime = datetime.strptime(new_match.group(1), '%Y-%m-%dT%H:%M:%S')
-                            # if old timestamp is more recent than new, then copy old to new
-                            if old_datetime > new_datetime:
-                                service_manager.service_items[service_index] = old_service_item
-                            break
-                    elif old_service_item['service_item'].name == 'custom' and service_item['service_item'].name == 'custom':
-                        """ we don't get actual slide content from the V2 PC API, so all we create by default is a 
-                        single slide with matching title and content.  If the content
-                        is different between the old serviceitem (previously imported
-                        from PC and the new content that we are importing now, then
-                        the assumption is that we updated this content and we want to 
-                        keep the old content after this update.  If we actually updated
-                        something on the PC site in this slide, it would have a
-                        different title because that is all we can get the v2API """
-                        if old_service_item['service_item'].title == service_item['service_item'].title:
-                            if old_service_item['service_item']._raw_frames != service_item['service_item']._raw_frames:
-                                service_manager.service_items[service_index] = old_service_item
-                            break
+                # get the current values of these values so I can set them back to this value
+                media_type_plugin = Registry().get(media_type)
+                # the variable suffix names below for "songs" is "song", so change media_type to song
+                media_type_suffix = media_type
+                if media_type == 'songs':
+                    media_type_suffix = 'song'
+                previous_remote_triggered = media_type_plugin.remote_triggered
+                previous_remote_value = getattr(media_type_plugin, "remote_{0}".format(media_type_suffix))
+                # turn on remote song feature to add to service
+                media_type_plugin.remote_triggered = True
+                setattr(media_type_plugin,"remote_{0}".format(media_type_suffix), openlp_id)
+                media_type_plugin.add_to_service(remote=openlp_id)
+                # reset values to their previous values
+                media_type_plugin.remote_triggered = previous_remote_triggered
+                setattr(media_type_plugin,"remote_{0}".format(media_type_suffix), previous_remote_value)
+                service_manager.main_window.increment_progress_bar()
                         
-        service_manager.main_window.finished_progress_bar()     
-        # select the first item
-        item = service_manager.service_manager_list.topLevelItem(0)
-        service_manager.service_manager_list.setCurrentItem(item)
-        service_manager.repaint_service_list(-1, -1)
+            if update:
+                for old_service_item in old_service_items:
+                    # see if this service_item contained within the current set of service items
+                    
+                    # see if we have this same value in the new service
+                    for service_index,service_item in enumerate(service_manager.service_items):
+                        # we can compare songs to songs and custom to custom but not between them
+                        if old_service_item['service_item'].name == 'songs' and service_item['service_item'].name == 'songs':
+                            if old_service_item['service_item'].audit == service_item['service_item'].audit:
+                                # get the timestamp from the xml of both the old and new and compare... 
+                                # modifiedDate="2018-06-30T18:44:35Z"
+                                old_match = re.search('modifiedDate="(.+?)Z*"', old_service_item['service_item'].xml_version)
+                                old_datetime = datetime.strptime(old_match.group(1), '%Y-%m-%dT%H:%M:%S')
+                                new_match = re.search('modifiedDate="(.+?)Z*"', service_item['service_item'].xml_version)
+                                new_datetime = datetime.strptime(new_match.group(1), '%Y-%m-%dT%H:%M:%S')
+                                # if old timestamp is more recent than new, then copy old to new
+                                if old_datetime > new_datetime:
+                                    service_manager.service_items[service_index] = old_service_item
+                                break
+                        elif old_service_item['service_item'].name == 'custom' and service_item['service_item'].name == 'custom':
+                            """ we don't get actual slide content from the V2 PC API, so all we create by default is a 
+                            single slide with matching title and content.  If the content
+                            is different between the old serviceitem (previously imported
+                            from PC and the new content that we are importing now, then
+                            the assumption is that we updated this content and we want to 
+                            keep the old content after this update.  If we actually updated
+                            something on the PC site in this slide, it would have a
+                            different title because that is all we can get the v2API """
+                            if old_service_item['service_item'].title == service_item['service_item'].title:
+                                if old_service_item['service_item']._raw_frames != service_item['service_item']._raw_frames:
+                                    service_manager.service_items[service_index] = old_service_item
+                                break
+                            
+            service_manager.main_window.finished_progress_bar()     
+            # select the first item
+            item = service_manager.service_manager_list.topLevelItem(0)
+            service_manager.service_manager_list.setCurrentItem(item)
+            service_manager.repaint_service_list(-1, -1)
+            service_manager.application.set_normal_cursor()
         self.done(QtWidgets.QDialog.Accepted)
-        service_manager.application.set_normal_cursor()
+            
 
     @property
     def application(self):
