b030e28ac1f0f2c31d36736132a37bdf0eb3c6f3
Stephen Brown <brownium@gmail.com>
2018-09-11 00:58:07 -0500

    don't allow build without version number

--- create_planningcenter_release.sh
+++ create_planningcenter_release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 VERSION=$1
+if [ "$1" == "" ]
+then
+  echo "Syntax: ./create_planningcetnter_release.sh <VERSION>"
+  exit 1
+fi
 echo plugin_version=\"$1\" >lib/version.py
 git add lib/version.py
 git commit -m "creating build version $1"
