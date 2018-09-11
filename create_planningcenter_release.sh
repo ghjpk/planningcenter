c046c85f1db92fa6f69095183431587b5ba74a5c
Stephen Brown <brownium@gmail.com>
2018-09-11 09:51:35 -0500

    add git tag at release time

--- create_planningcenter_release.sh
+++ create_planningcenter_release.sh
@@ -8,4 +8,5 @@ fi
 echo plugin_version=\"$1\" >lib/version.py
 git add lib/version.py
 git commit -m "creating build version $1"
+git tag "$1"
 git archive -o planningcenter_$VERSION.zip --prefix=planningcenter/ -9 HEAD
