1bfe35cc9e74b49ee36898bf56f956e6f80cd97b
Stephen Brown <brownium@gmail.com>
2018-09-29 00:12:42 -0500

    create release builds from 'master' rather than 'HEAD'

--- create_planningcenter_release.sh
+++ create_planningcenter_release.sh
@@ -9,4 +9,4 @@ echo plugin_version=\"$1\" >lib/version.py
 git add lib/version.py
 git commit -m "creating build version $1"
 git tag "$1"
-git archive -o planningcenter_$VERSION.zip --prefix=planningcenter/ -9 HEAD
+git archive -o planningcenter_$VERSION.zip --prefix=planningcenter/ -9 master
