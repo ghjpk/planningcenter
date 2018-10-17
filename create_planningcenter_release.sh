3b085b64ee5e041851bf9972b853b6923841c42f
John Kirkland <13470641+ghjpk@users.noreply.github.com>
2018-10-17 14:09:30 -0500

    create annotated tag

--- create_planningcenter_release.sh
+++ create_planningcenter_release.sh
@@ -8,5 +8,5 @@ fi
 echo plugin_version=\"$1\" >lib/version.py
 git add lib/version.py
 git commit -m "creating build version $1"
-git tag "$1"
+git tag -a "$1" -m "creating build version $1"
 git archive -o planningcenter_$VERSION.zip --prefix=planningcenter/ -9 HEAD
